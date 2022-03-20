563698c8ef99a8fbd2ddb5d3a3c5107047f01b7e
Sergei Viktorov <sergei_viktorov@mail.ru>
2022-03-20 19:00:50 +0400

    crashed in test limitNoVKPay

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -242,7 +242,7 @@ class MainKtTest {
         val cardType = "Maestro"
         val amount: Long = 150000
         val lastTransfer: Long = 450000
-        val expectedCommissionValue = false
+        val expectedCommissionValue = true
         //act
         val actualCommissionValue = ru.netology.limit(cardType, lastTransfer, amount)
         //assert
